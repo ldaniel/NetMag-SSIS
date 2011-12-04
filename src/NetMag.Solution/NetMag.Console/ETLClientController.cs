e64c4d4bee475cdcc8610e2961a1a1a86db666fd
ldaniel <contato@leandrodaniel.com>
2011-12-04 17:55:31 -0200

    Upload

---
+++ src/NetMag.Solution/NetMag.Console/ETLClientController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.SqlServer.Dts.Runtime;
+
+namespace NetMag.Console
+{
+    public class DTSX
+    {
+        public string ExecutarPacoteLocal(string caminhoPacote)
+        {   
+            Application app = new Application();
+            Package pacote = app.LoadPackage(caminhoPacote, null);
+            DTSExecResult resultado = pacote.Execute();
+
+            return resultado.ToString();
+        }
+
+        public string ExecutarPacoteRemoto(string connString, string nomeJob)
+        {
+            string resultado;
+
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand command = new SqlCommand("sp_start_job", conn);
+            command.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter jobReturnValue = new SqlParameter(
+                "@RETURN_VALUE", SqlDbType.Int);
+            jobReturnValue.Direction = ParameterDirection.ReturnValue;
+            command.Parameters.Add(jobReturnValue);
+
+            SqlParameter jobParameter = new SqlParameter(
+                "@job_name", SqlDbType.VarChar);
+            jobParameter.Direction = ParameterDirection.Input;
+            command.Parameters.Add(jobParameter);
+            jobParameter.Value = nomeJob;
+
+            try
+            {
+                conn.Open();
+                command.ExecuteNonQuery();
+                int jobResultado = 
+                    (Int32)command.Parameters["@RETURN_VALUE"].Value;
+
+                switch (jobResultado)
+                {
+                    case 0:
+                        resultado = "Pacote iniciado com sucesso.";
+                        break;
+                    default:
+                        resultado = "Pacote falhou ao iniciar.";
+                        break;
+                }
+            }
+            catch (Exception exception)
+            {
+                resultado = exception.ToString();
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return resultado;
+        }
+    }
+} 
---
+++ src/NetMag.Solution/NetMag.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetMag.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DTSX client = new DTSX();
+            string result;
+
+            result = client.ExecutarPacoteLocal(@"C:\Documents and Settings\Temporario\Desktop\Artigos\Solution\NetMag.Solution\NetMag.ETL\bin\LoadReviewer.dtsx");
+
+            System.Console.Write(result);
+            System.Console.ReadKey();
+
+            result = client.ExecutarPacoteRemoto(@"Data Source=(local);Initial Catalog=msdb;Integrated Security=SSPI", "LoadReviewerJob");
+            System.Console.Write(result);
+            System.Console.ReadKey();            
+        }
+    }
+}
